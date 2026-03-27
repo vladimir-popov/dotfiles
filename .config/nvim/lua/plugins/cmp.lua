f39a96a28c3c6537c4d9fad025d857cacfde885c
Vladimir Popov <vladimir@dokwork.ru>
2026-03-27 19:48:10 +0400

    Use borders for cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -89,6 +89,10 @@ return {
                     require('luasnip').lsp_expand(args.body)
                 end,
             },
+            window = {
+                completion = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
+            },
             mapping = {
                 ['<C-d>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-f>'] = cmp.mapping.scroll_docs(4),
