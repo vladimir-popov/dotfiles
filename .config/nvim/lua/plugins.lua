0521c95a60f89bfca8a8912cb0a9f3605533a7e2
Vladimir Popov <vladimir@dokwork.ru>
2026-07-25 10:40:13 +0400

    csview plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -344,5 +344,26 @@ au FileType plantuml let g:plantuml_previewer#plantuml_jar_path = get(
         config = function()
             vim.g.table_mode_corner = '|'
         end,
+    },
+    -- CSV
+    {
+        "hat0uma/csvview.nvim",
+        opts = {
+            parser = { comments = { "#", "//" } },
+            keymaps = {
+                -- Text objects for selecting fields
+                textobject_field_inner = { "ic", mode = { "o", "x" } },
+                textobject_field_outer = { "ac", mode = { "o", "x" } },
+                -- Excel-like navigation:
+                -- Use <Tab> and <S-Tab> to move horizontally between fields.
+                -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+                -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+                jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+                jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+                jump_next_row = { "<Enter>", mode = { "n", "v" } },
+                jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+            },
+        },
+        cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
     }
 }
